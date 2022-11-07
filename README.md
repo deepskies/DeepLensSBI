29dfeb313819f0bfe07f5b4735fd846b03362586
Ashwin Samudre <capnoi8@gmail.com>
2022-11-07 11:46:53 -0800

    Readme updated

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DeepSLEEP
 Strong Lensing parameter inference with SBI
+
+Current ground-based cosmological surveys, such as the Dark Energy Survey (DES), are predicted to discover thousands of galaxy-scale strong lenses, while future surveys, such as the Vera Rubin Observatory Legacy Survey of Space and Time (LSST) will increase that number by 1-2 orders of magnitude.
+The large number of strong lenses discoverable in future surveys will make strong lensing a highly competitive and complementary cosmic probe.
+
+To leverage the increased statistical power of the lenses that will be discovered through upcoming surveys, automated lens analysis techniques are necessary. We present two Simulation-Based Inference (SBI) approaches for lens parameter estimation of galaxy-galaxy lenses. We demonstrate the successful application of Neural Posterior Estimation (NPE) to automate the inference of a 12-parameter lens mass model for DES-like ground-based imaging data. We compare our NPE constraints to a Bayesian Neural Network (BNN) and find that it outperforms the BNN, producing posterior distributions that are for the most part both more accurate and more precise; in particular, several source-light model parameters are systematically biased in the BNN implementation.
+
+Here we provide implementation for both NPE and BNN approaches presented in our paper.
--- src/fit_params.py
+++ src/fit_params.py
@@ -1,25 +1,25 @@
 import numpy as np
 import torch
 
+def fit_parameters():
+    limits = np.array([prior_low, prior_high]).T
 
-limits = np.array([prior_low, prior_high]).T
+    # A bit cumbersome, but we need to make a vector of the true parameter values.
+    theta_E = observed.CONFIGURATION_1_metadata['PLANE_1-OBJECT_1-MASS_PROFILE_1-theta_E-g']
+    le1 = observed.CONFIGURATION_1_metadata['PLANE_1-OBJECT_1-MASS_PROFILE_1-e1-g']
+    le2 = observed.CONFIGURATION_1_metadata['PLANE_1-OBJECT_1-MASS_PROFILE_1-e2-g']
+    smag = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-magnitude-g']
+    x = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-center_x-g']
+    y = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-center_y-g']
+    R = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-R_sersic-g']
+    n = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-n_sersic-g']
+    se1 = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-e1-g']
+    se2 = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-e2-g']
 
-# A bit cumbersome, but we need to make a vector of the true parameter values. 
-theta_E = observed.CONFIGURATION_1_metadata['PLANE_1-OBJECT_1-MASS_PROFILE_1-theta_E-g']
-le1 = observed.CONFIGURATION_1_metadata['PLANE_1-OBJECT_1-MASS_PROFILE_1-e1-g']
-le2 = observed.CONFIGURATION_1_metadata['PLANE_1-OBJECT_1-MASS_PROFILE_1-e2-g']
-smag = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-magnitude-g']
-x = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-center_x-g']
-y = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-center_y-g']
-R = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-R_sersic-g']
-n = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-n_sersic-g']
-se1 = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-e1-g']
-se2 = observed.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-e2-g']
+    true_parameter = torch.tensor([theta_E[0], le1[0], le2[0], smag[0], x[0], y[0], R[0], n[0], se1[0], se2[0]])
 
-true_parameter = torch.tensor([theta_E[0], le1[0], le2[0], smag[0], x[0], y[0], R[0], n[0], se1[0], se2[0]])
-
-# Now let's make a vector of the median (one definition of "best-fit") values of the posterior for each parameter
-best_fit = []
-for i in range(10):
-  best_fit.append(np.median(samples[:,i]))
-best_fit_t = torch.tensor(best_fit)
+    # Now let's make a vector of the median (one definition of "best-fit") values of the posterior for each parameter
+    best_fit = []
+    for i in range(10):
+      best_fit.append(np.median(samples[:,i]))
+    best_fit_t = torch.tensor(best_fit)
