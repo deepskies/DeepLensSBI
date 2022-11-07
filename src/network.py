fdd4f8069b75ac263dfd81a5357627f84ce1185e
Ashwin Samudre <capnoi8@gmail.com>
2022-11-07 11:39:40 -0800

    NPE source files added

---
+++ src/network.py
@@ -0,0 +1,25 @@
+import torch
+from torchsummary import summary
+
+# Summary Network module - with summary
+class SummaryNet(nn.Module): 
+    
+    def __init__(self): 
+        super().__init__()
+        # 2D convolutional layer
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2)
+        # Maxpool layer that reduces 32x32 image to 4x4
+        self.pool = nn.MaxPool2d(kernel_size=8, stride=8)
+        # Fully connected layer taking as input the 6 flattened output arrays from the maxpooling layer
+        self.fc = nn.Linear(in_features=6*4*4, out_features=10) 
+        
+    def forward(self, x):
+        x = x.view(-1, 1, 32, 32)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = x.view(-1, 6*4*4)
+        x = F.relu(self.fc(x))
+        return x
+
+embedding_net = SummaryNet()
+
+summary(embedding_net, (1, 32, 32))
---
+++ src/train.py
@@ -0,0 +1,47 @@
+import torch
+from sbi import utils, inference
+from sbi.inference import SNPE, SNLE, SNRE
+
+
+prior_low = [1.0, 0.00, 0.00, 19.9, -0.1, -0.1, 1.0, 3, 0.0, 0.0]
+prior_high = [4.0, 0.05, 0.05, 20.1, 0.1, 0.1, 2.0, 5, 0.05, 0.05]
+
+prior = utils.BoxUniform(low=torch.tensor(prior_low), 
+                             high=torch.tensor(prior_high))                           
+
+# instantiate the neural density estimator
+neural_posterior = utils.posterior_nn(model='maf', 
+                                      embedding_net=embedding_net,
+                                      hidden_features=10,
+                                      num_transforms=2)
+
+# setup the inference procedure with the SNPE-C procedure
+inference = SNPE(prior=prior, density_estimator=neural_posterior)
+
+# Reshape the images so each image is a vector. Note: The first number should be the same as the size of the dataset in training_sim.yaml 
+images = torch.tensor(dataset.CONFIGURATION_1_images.reshape(1000,1,1024),dtype=torch.float32)[:,0]
+
+# Now let's choose the parameters we are interested in training the SBI network on. 
+# You can see the complete list of available parameters in deeplenstronomy by printing dataset.CONFIGURATION_1_metadata.keys(). 
+# In many cases we are only interested in a subset of all the available parameters
+
+# These 3 are lens mass parameters of a SIE: Einstein radius and ellipticity. positions fixed to (0,0)
+theta_E = dataset.CONFIGURATION_1_metadata['PLANE_1-OBJECT_1-MASS_PROFILE_1-theta_E-g']
+le1 = dataset.CONFIGURATION_1_metadata['PLANE_1-OBJECT_1-MASS_PROFILE_1-e1-g']
+le2 = dataset.CONFIGURATION_1_metadata['PLANE_1-OBJECT_1-MASS_PROFILE_1-e2-g']
+
+# These are source light parameters - magnitude and parameters of a Sersic profile. 
+smag = dataset.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-magnitude-g']
+x = dataset.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-center_x-g']
+y = dataset.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-center_y-g']
+R = dataset.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-R_sersic-g']
+n = dataset.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-n_sersic-g']
+se1 = dataset.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-e1-g']
+se2 = dataset.CONFIGURATION_1_metadata['PLANE_2-OBJECT_1-LIGHT_PROFILE_1-e2-g']
+
+# Now let's put them in a tensor form that SBI can read.
+theta = torch.tensor(np.array([theta_E,le1,le2,smag,x,y,R,n,se1,se2]).T,dtype=torch.float32)
+
+# Now that we have both the simulated images and parameters defined properly, we can train the SBI. 
+density_estimator = inference.append_simulations(theta,images).train()
+posterior = inference.build_posterior(density_estimator)
